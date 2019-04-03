70276fecacd6e97beeef84f8df1466ffed497def
Stefan Lobbenmeier <36509607+StefanLobbenmeier@users.noreply.github.com>
2019-04-03 12:31:53 +0200

    Resolved Dart Analysis errors (#138)
    * Resolved Dat Analysis errors
    
    * Resolved Dart Analysis errors

--- lib/src/chewie_player.dart
+++ lib/src/chewie_player.dart
@@ -266,8 +266,7 @@ class ChewieController extends ChangeNotifier {
   final ChewieRoutePageBuilder routePageBuilder;
 
   static ChewieController of(BuildContext context) {
-    final _ChewieControllerProvider chewieControllerProvider =
-        context.inheritFromWidgetOfExactType(_ChewieControllerProvider) as _ChewieControllerProvider;
+    final chewieControllerProvider = _ChewieControllerProvider.of(context);
 
     return chewieControllerProvider.controller;
   }
@@ -351,6 +350,8 @@ class _ChewieControllerProvider extends InheritedWidget {
         super(key: key, child: child);
 
   final ChewieController controller;
+
+  static _ChewieControllerProvider of(BuildContext context) => context.inheritFromWidgetOfExactType(_ChewieControllerProvider) as _ChewieControllerProvider;
 
   @override
   bool updateShouldNotify(_ChewieControllerProvider old) =>
