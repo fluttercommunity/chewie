8f5d9f0793d83b0121be170ba7c1a944d6d982ea
Rashed Mazumder <yaan.uchiha29@gmail.com>
2019-11-11 15:08:13 +0600

    git video player path added

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/phillywiggins/lab/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/phillywiggins/lab/chewie/example"
+export "FLUTTER_ROOT=/Users/rashedoz/softwares/Softs/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/rashed_personal/FLutter_Core_Dev/CustomChewie/chewie/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/phillywiggins/lab/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/rashedoz/softwares/Softs/flutter/bin/cache/artifacts/engine/ios"
