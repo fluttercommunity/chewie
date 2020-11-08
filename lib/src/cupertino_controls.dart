9b4404ca698edc0bc973314568f342dfda14349a
Ahmadre <rebar.ahmad@telexiom.de>
2020-11-08 13:05:21 +0100

    Prepare with `dartfmt -w .` for publication

--- lib/src/cupertino_controls.dart
+++ lib/src/cupertino_controls.dart
@@ -496,15 +496,12 @@ class _CupertinoControlsState extends State<CupertinoControls> {
   }
 
   void _playPause() {
-      bool isFinished;
-      if( _latestValue.duration != null)
-      {
-        isFinished = _latestValue.position >= _latestValue.duration;
-      }
-      else
-      {
-        isFinished = false;
-      }
+    bool isFinished;
+    if (_latestValue.duration != null) {
+      isFinished = _latestValue.position >= _latestValue.duration;
+    } else {
+      isFinished = false;
+    }
 
     setState(() {
       if (controller.value.isPlaying) {
