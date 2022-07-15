39f887694a7e9b19bfdf5955100920548629e72b
Geevies <ggrobler447@gmail.com>
2022-07-15 14:26:11 +0200

    Add transformationController to Interactive Viewer

--- lib/src/chewie_player.dart
+++ lib/src/chewie_player.dart
@@ -262,6 +262,7 @@ class ChewieController extends ChangeNotifier {
     this.optionsBuilder,
     this.additionalOptions,
     this.showControls = true,
+    this.transformationController,
     this.zoomAndPan = false,
     this.maxScale = 2.5,
     this.subtitle,
@@ -307,6 +308,7 @@ class ChewieController extends ChangeNotifier {
     Future<void> Function(BuildContext, List<OptionItem>)? optionsBuilder,
     List<OptionItem> Function(BuildContext)? additionalOptions,
     bool? showControls,
+    TransformationController? transformationController,
     bool? zoomAndPan,
     double? maxScale,
     Subtitles? subtitle,
@@ -438,6 +440,9 @@ class ChewieController extends ChangeNotifier {
 
   /// Whether or not to show the controls at all
   final bool showControls;
+
+  /// Controller to pass into the [InteractiveViewer] component
+  final TransformationController? transformationController;
 
   /// Whether or not to allow zooming and panning
   final bool zoomAndPan;
