da56bfc6718b751c9e1f95986dd5cdffe31c6f4a
sunsun <sunsun@gmail.com>
2022-09-05 01:52:32 +0800

    Fix Platform._operatingSystem exception generated by running web

--- example/lib/app/app.dart
+++ example/lib/app/app.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:chewie/chewie.dart';
 import 'package:chewie_example/app/theme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:video_player/video_player.dart';
@@ -311,7 +312,10 @@ class _ChewieDemoState extends State<ChewieDemo> {
                 ),
               ],
             ),
-            if (Platform.isAndroid)
+            // When running the web, "Platform.isAndroid" will cause "Platform._operatingSystem" exception.
+            // So instead use "defaultTargetPlatform == TargetPlatform.android"
+            // if (Platform.isAndroid)
+            if (defaultTargetPlatform == TargetPlatform.android)
               ListTile(
                 title: const Text("Delay"),
                 subtitle: DelaySlider(
