4980434887dd34c34c9c0a07a8440dd604098465
SHOURYA SHIKHAR <danger.ahead@pm.me>
2022-11-04 12:16:38 +0530

    Fixed unsupported operation: Platform._operatingSystem

--- example/lib/app/app.dart
+++ example/lib/app/app.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-
 import 'package:chewie/chewie.dart';
 import 'package:chewie_example/app/theme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:video_player/video_player.dart';
@@ -311,7 +310,7 @@ class _ChewieDemoState extends State<ChewieDemo> {
                 ),
               ],
             ),
-            if (Platform.isAndroid)
+            if (defaultTargetPlatform == TargetPlatform.android)
               ListTile(
                 title: const Text("Delay"),
                 subtitle: DelaySlider(
