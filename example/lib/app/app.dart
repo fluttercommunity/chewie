d2b1a312f16326b29866346fb440d9a53b856572
Diego Tori <diegotoridoesandroid@gmail.com>
2023-07-02 16:30:22 -0400

    Issue #729. Migrated wakelock to wakelock_plus. Also upgraded all other dependencies.

--- example/lib/app/app.dart
+++ example/lib/app/app.dart
@@ -49,9 +49,9 @@ class _ChewieDemoState extends State<ChewieDemo> {
 
   Future<void> initializePlayer() async {
     _videoPlayerController1 =
-        VideoPlayerController.network(srcs[currPlayIndex]);
+        VideoPlayerController.networkUrl(Uri.parse(srcs[currPlayIndex]));
     _videoPlayerController2 =
-        VideoPlayerController.network(srcs[currPlayIndex]);
+        VideoPlayerController.networkUrl(Uri.parse(srcs[currPlayIndex]));
     await Future.wait([
       _videoPlayerController1.initialize(),
       _videoPlayerController2.initialize()
