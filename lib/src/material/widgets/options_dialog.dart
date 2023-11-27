d1a99e1967544eca691c67de83038a4e92ad85dd
koutaro-masaki <koutaro.masaki@glucose.jp>
2023-11-27 22:06:57 +0900

    fix: lint issue

--- lib/src/material/widgets/options_dialog.dart
+++ lib/src/material/widgets/options_dialog.dart
@@ -28,9 +28,7 @@ class _OptionsDialogState extends State<OptionsDialog> {
             itemCount: widget.options.length,
             itemBuilder: (context, i) {
               return ListTile(
-                onTap: widget.options[i].onTap != null
-                    ? widget.options[i].onTap!
-                    : null,
+                onTap: widget.options[i].onTap,
                 leading: Icon(widget.options[i].iconData),
                 title: Text(widget.options[i].title),
                 subtitle: widget.options[i].subtitle != null
