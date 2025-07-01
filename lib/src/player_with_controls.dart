f227c5059f94b1c9f52ab17453863984b1466571
Diego Tori <diego.tori@greenfence.com>
2025-06-30 20:22:33 -0400

    Ran dart format on library.

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -36,10 +36,12 @@ class PlayerWithControls extends StatelessWidget {
       final playerNotifier = context.read<PlayerNotifier>();
       final child = Stack(
         children: [
-          if (chewieController.placeholder != null) chewieController.placeholder!,
+          if (chewieController.placeholder != null)
+            chewieController.placeholder!,
           Center(
             child: AspectRatio(
-              aspectRatio: chewieController.aspectRatio ??
+              aspectRatio:
+                  chewieController.aspectRatio ??
                   chewieController.videoPlayerController.value.aspectRatio,
               child: VideoPlayer(chewieController.videoPlayerController),
             ),
@@ -74,16 +76,21 @@ class PlayerWithControls extends StatelessWidget {
         ],
       );
 
-      if (chewieController.zoomAndPan || chewieController.transformationController != null) {
+      if (chewieController.zoomAndPan ||
+          chewieController.transformationController != null) {
         return InteractiveViewer(
           transformationController: chewieController.transformationController,
           maxScale: chewieController.maxScale,
           panEnabled: chewieController.zoomAndPan,
           scaleEnabled: chewieController.zoomAndPan,
           onInteractionUpdate:
-              chewieController.zoomAndPan ? (_) => playerNotifier.hideStuff = true : null,
+              chewieController.zoomAndPan
+                  ? (_) => playerNotifier.hideStuff = true
+                  : null,
           onInteractionEnd:
-              chewieController.zoomAndPan ? (_) => playerNotifier.hideStuff = false : null,
+              chewieController.zoomAndPan
+                  ? (_) => playerNotifier.hideStuff = false
+                  : null,
           child: child,
         );
       }
