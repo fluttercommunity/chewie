1b3d94c361a261c1a175f6c926ddf20dc2f02e22
Jakob Lehofer <jakob@seadev-studios.com>
2026-03-12 08:34:55 +0100

    Use dart.library.js_interop instead of deprecated dart.library.html

--- lib/src/helpers/fullscreen_controller.dart
+++ lib/src/helpers/fullscreen_controller.dart
@@ -1,1 +1,1 @@
-export 'fullscreen_controller_unsupported.dart' if (dart.library.html) 'fullscreen_controller_web.dart';
+export 'fullscreen_controller_unsupported.dart' if (dart.library.js_interop) 'fullscreen_controller_web.dart';
