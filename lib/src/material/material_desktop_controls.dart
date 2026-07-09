bec8436117be0d556089ab752b6c7783c903c98f
Ortes <malo.allee@gmail.com>
2026-07-09 14:00:32 -0400

    feat: toggle fullscreen with the F key on desktop
    Add an F keyboard shortcut to the desktop controls that toggles
    fullscreen (respecting allowFullScreen), complementing the existing
    Esc-to-exit shortcut.

--- lib/src/material/material_desktop_controls.dart
+++ lib/src/material/material_desktop_controls.dart
@@ -107,6 +107,11 @@ class _MaterialDesktopControlsState extends State<MaterialDesktopControls>
       if (chewieController.isFullScreen) {
         _onExpandCollapse();
       }
+    } else if (event is KeyDownEvent &&
+        event.logicalKey == LogicalKeyboardKey.keyF) {
+      if (chewieController.allowFullScreen) {
+        _onExpandCollapse();
+      }
     }
   }
 
